b8e4f3b5bf9ff2ed8e390df1ac4271a0a59deb7c
Richet <shanon.richet@gmail.com>
2021-11-17 16:58:19 +0100

    add disponibilités

--- src/js/shanon.js
+++ src/js/shanon.js
@@ -26,7 +26,14 @@ fetch("http://localhost:3000/api/events/")
             section.appendChild(sectionDate)
             var possibleDate= document.createElement('p')
             possibleDate.innerHTML= date.date
-            sectionDate.appendChild(possibleDate)         
+            sectionDate.appendChild(possibleDate) 
+
+            for (const x of date.attendees) {
+                console.log(x)
+                var available= document.createElement('p')
+                available.innerHTML= x.available
+                sectionDate.appendChild(available) 
+            }                 
         }
         for (const names of event.dates[0].attendees) {
         var guestList= document.createElement('section')
@@ -34,14 +41,11 @@ fetch("http://localhost:3000/api/events/")
         var guest= document.createElement('p')
         guest.innerHTML= names.name
 
-        var attend= document.createElement('p')
-        attend.innerHTML= names.available
         guestList.appendChild(guest)
 
-        guestList.setAttribute('class', 'guest')
-        guestList.appendChild(attend)
-        }
     }
+
+}
 })
 
 
