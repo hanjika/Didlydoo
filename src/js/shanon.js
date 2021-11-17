29b613b144705f0b6176ce71c4e328ee479ab510
Richet <shanon.richet@gmail.com>
2021-11-17 16:06:47 +0100

    no change

--- src/js/shanon.js
+++ src/js/shanon.js
@@ -1,31 +1,15 @@
 const main= document.querySelector('main')
 const eventSection= document.querySelector('.eventSection')
 const guestSection= document.querySelector('.guestSection')
-const submit= document.querySelector('.submit')
-const submit2= document.querySelector('.submit2')
 const inputEvent= document.querySelector('.events')
 const inputGuest= document.querySelector('.guests')
-
-submit.onclick= () => {
-    var eventName= document.createElement('p')
-    eventName.innerHTML= inputEvent.value
-    eventSection.appendChild(eventName)
-}
-submit2.onclick= () => {
-    var guestName= document.createElement('p')
-    guestName.innerHTML= inputGuest.value
-    guestSection.appendChild(guestName)
-}
 
 fetch("http://localhost:3000/api/events/")
 .then(response => response.json())
 .then(json => {
-    console.log(json)
     for (const event of json) {
-
         var article= document.createElement('article')
         eventSection.appendChild(article)
-
         var name= document.createElement('h3')
         name.innerHTML= event.name
         article.appendChild(name)
@@ -35,21 +19,36 @@ fetch("http://localhost:3000/api/events/")
         article.appendChild(description)
         var section= document.createElement('section')
         section.setAttribute('class', 'dates')
-        article.appendChild(section)
-        for (const date of event.dates){
+        article.appendChild(section) 
+             
+        for (const date of event.dates) {
+            const sectionDate= document.createElement('section')
+            section.appendChild(sectionDate)
             var possibleDate= document.createElement('p')
             possibleDate.innerHTML= date.date
-            section.appendChild(possibleDate)
-            }
+            sectionDate.appendChild(possibleDate)         
+        }
         for (const names of event.dates[0].attendees) {
-        console.log(names)
-        var section2= document.createElement('section')
-        section2.setAttribute('class', 'guest')
-        article.appendChild(section2)
+        var guestList= document.createElement('section')
+        article.appendChild(guestList)
         var guest= document.createElement('p')
         guest.innerHTML= names.name
-        section2.appendChild(guest)
+
+        var attend= document.createElement('p')
+        attend.innerHTML= names.available
+        guestList.appendChild(guest)
+
+        guestList.setAttribute('class', 'guest')
+        guestList.appendChild(attend)
         }
+
+        for (const names of event.dates[1].attendees) {    
+            var attend= document.createElement('p')
+            attend.innerHTML= names.available
+
+            guestList.appendChild(attend)
+            console.log(names)
+            }
     }
 })
 
